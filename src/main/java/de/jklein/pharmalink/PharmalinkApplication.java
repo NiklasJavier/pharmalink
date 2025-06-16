91e63a1c5d2903e1e22da8a570f54756aa37d565
Niklas Klein <59505702+NiklasJavier@users.noreply.github.com.>
2025-06-16 23:23:58 +0200

    chore: Refactor project structure and integrate Hyperledger Fabric Gateway
    - Removed `Example.java` and replaced with a new `App.java` class for improved functionality and adherence to best practices.
    - Renamed `PharmalinkApplication` to `App` in the main Spring Boot application.
    - Updated `build.gradle` to include dependencies for Hyperledger Fabric Gateway and aligned with gRPC and Gson libraries.
    - Enhanced configuration by introducing `application.properties` and populating with Fabric Gateway settings.
    - Deleted `application.properties` legacy file under `src-old`.
    - Simplified Crypto material paths and implemented flexibility using environment variables.

--- src/main/java/de/jklein/pharmalink/PharmalinkApplication.java
+++ src/main/java/de/jklein/pharmalink/PharmalinkApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PharmalinkApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PharmalinkApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
