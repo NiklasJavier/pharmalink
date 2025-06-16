9e563a198be9e7913f24ffc80cfe39c407bf0510
Niklas Klein <59505702+NiklasJavier@users.noreply.github.com.>
2025-06-16 18:21:27 +0200

    chore: Add chaincode deployment to Fabric setup script
    - Introduced `deployChaincode` function to handle Gradle build and chaincode deployment.
    - Enhanced help output with new chaincode deployment mode (`cc`).
    - Refactored and tidied whitespace and comments for consistency.

--- scripts/fabric_setup_test.sh
+++ scripts/fabric_setup_test.sh
@@ -5,60 +5,61 @@ FABRIC_VERSION="2.5.13"
 CA_VERSION="1.5.15"
 
 function networkUp() {
-	networkDown
-	cd $SRC_DIR/..
-	curl -sSLO https://raw.githubusercontent.com/hyperledger/fabric/main/scripts/install-fabric.sh && chmod +x install-fabric.sh
-	./install-fabric.sh --fabric-version ${FABRIC_VERSION} --ca-version ${CA_VERSION}
+  networkDown
+  cd $SRC_DIR/..
+  curl -sSLO https://raw.githubusercontent.com/hyperledger/fabric/main/scripts/install-fabric.sh && chmod +x install-fabric.sh
+  ./install-fabric.sh --fabric-version ${FABRIC_VERSION} --ca-version ${CA_VERSION}
 
   cp -r ./common/organizations ./fabric-samples/test-network/
 
-  # Weitere Befehle welchen beim SetUp verwendet werden können
-  #
-  # echo "./startNetwork up -v 2.4.1"
-  #
-  # Wichtige Verzeichnisse:
-  #
-  # ./fabric-samples/test-network/organizations
-  # ./fabric-samples/test-network/compose
-  # ./fabric-samples/test-network/configtx
-  #
-  ## Meine Überschreibungen (Work in Progress (WIP))
-  #
-  #cp -r ./common/template/organizations ./fabric-samples/test-network/
-  #cp -r ./common/template/compose ./fabric-samples/test-network/
-  #cp -r ./common/template/configtx ./fabric-samples/test-network/
-  #cp ./common/template/network.sh ./fabric-samples/test-network/network.sh
-  #cp ./common/template/envVar.sh ./fabric-samples/test-network/scripts/envVar.sh
-  #cp ./common/template/createChannel.sh ./fabric-samples/test-network/scripts/createChannel.sh
-  #cp ./common/template/orderer.sh ./fabric-samples/test-network/scripts/orderer.sh
-  #cp ./common/template/setAnchorPeer.sh ./fabric-samples/test-network/scripts/setAnchorPeer.sh
-  #cp ./common/template/configUpdate.sh ./fabric-samples/test-network/scripts/configUpdate.sh
+  cd fabric-samples
 
-	cd fabric-samples
-
-	# default to fabric-samples main branch unless v2.2.x is downloaded
-	if [[ "${FABRIC_VERSION}" =~ ^2\.2\.[0-9]+$ ]]; then
-		git checkout release-2.2
-	fi
-	cd test-network
-	./network.sh up createChannel -ca -c pharmalink -s couchdb
-
-	# Uncomment to install asset-transfer-basic or fabcar
-	# ./network.sh deployCC -ccn basicgo -ccp ../asset-transfer-basic/chaincode-go/ -ccl go
-	# ./network.sh deployCC -ccn fabcar -ccv 1 -cci initLedger -ccl go -ccp ../chaincode/fabcar/go/
+  # default to fabric-samples main branch unless v2.2.x is downloaded
+  if [[ "${FABRIC_VERSION}" =~ ^2\.2\.[0-9]+$ ]]; then
+   git checkout release-2.2
+  fi
+  cd test-network
+  ./network.sh up createChannel -ca -c pharmalink -s couchdb
 }
 
 function networkDown() {
-	if [[ -d $SRC_DIR/../fabric-samples/test-network ]]; then
-		cd $SRC_DIR/../fabric-samples/test-network
-		./network.sh down
-		rm -fr $SRC_DIR/../fabric-samples
-	fi
+  if [[ -d $SRC_DIR/../fabric-samples/test-network ]]; then
+   cd $SRC_DIR/../fabric-samples/test-network
+   ./network.sh down
+   rm -fr $SRC_DIR/../fabric-samples
+  fi
 }
+
+# NEUE FUNKTION HINZUGEFÜGT
+function deployChaincode() {
+  echo "## 1. Compiling and packaging chaincode with Gradle..."
+  # Führt build und installDist aus. -p zeigt auf das Projektverzeichnis.
+  # Wir nehmen an, das Skript wird aus dem übergeordneten Ordner von "chaincode" ausgeführt.
+  gradle -p $SRC_DIR/../chaincode/pharmalink_chaincode_main/ installDist
+
+  # Prüfen, ob der Gradle-Befehl erfolgreich war
+  if [ $? -ne 0 ]; then
+    echo "!!! ERROR: Gradle build failed."
+    exit 1
+  fi
+  echo "## Gradle build successful."
+
+  echo ""
+  echo "## 2. Deploying chaincode to the Fabric network..."
+  $SRC_DIR/../fabric-samples/test-network/network.sh deployCC -ccn pharmalink_chaincode_main -ccp $SRC_DIR/../../chaincode/pharmalink_chaincode_main -ccl java
+
+  if [ $? -ne 0 ]; then
+    echo "!!! ERROR: Chaincode deployment failed."
+    exit 1
+  fi
+  echo "## Chaincode deployment successful."
+}
+
 
 function printHelp() {
  echo "./fabric_setup_test up"
  echo "./fabric_setup_test down"
+ echo "./fabric_setup_test cc" # HILFE-TEXT ERWEITERT
 }
 
 if [[ $# -lt 1 ]] ; then
@@ -78,9 +79,9 @@ while [[ $# -ge 1 ]] ; do
     ;;
   -v )
     FABRIC_VERSION="$2"
-	if [ ! -z "$3" ];then
-	    CA_VERSION="$3"
-	fi
+  if [ ! -z "$3" ];then
+      CA_VERSION="$3"
+  fi
     shift
     ;;
   esac
@@ -91,6 +92,9 @@ if [ "${MODE}" == "up" ]; then
   networkUp
 elif [ "${MODE}" == "down" ]; then
   networkDown
+# NEUER MODUS HINZUGEFÜGT
+elif [ "${MODE}" == "cc" ]; then
+  deployChaincode
 else
   printHelp
   exit 1
